019422644b2be7917325e230407c643ffb81dbff
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2024-04-04 12:07:38 -0400

    karma testing

--- src/app/add-employee/add-employee-modal.component.spec.ts
+++ src/app/add-employee/add-employee-modal.component.spec.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { MatDialogRef } from '@angular/material/dialog';
 import { AddEmployeeModalComponent } from './add-employee-modal.component';
+import { By } from '@angular/platform-browser';
 
 @Component({selector: 'app-mat-dialog-ref', template: ''})
 class DialogRef {
@@ -28,14 +29,14 @@ describe('AddEmployeeModalComponent', () => {
         fixture = TestBed.createComponent(AddEmployeeModalComponent);
         component = fixture.componentInstance;
         component.employee = {
-          id: 1,
-          firstName: 'first',
-          lastName: 'last',
-          position: 'jobTitle'
+          id: null,
+          firstName: '',
+          lastName: '',
+          position: ''
         };
     });
 
-    it('should create the update modal component', async(() => {
+    it('should create the add employee modal component', async(() => {
         expect(component).toBeTruthy();
     }));
 
@@ -49,5 +50,69 @@ describe('AddEmployeeModalComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain('Add New Employee');
+    }));
+
+    it('should input value in first name', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#firstName'));
+            let el = input.nativeElement;
+
+            expect(el.value).toBe('');
+
+            el.value = 'Kaylyn';
+            el.dispatchEvent(new Event('input'));
+
+            expect(el.value).toBe('Kaylyn');
+        })
+    }));
+
+    it('should input value in last name', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#lastName'));
+            let el = input.nativeElement;
+
+            expect(el.value).toBe('');
+
+            el.value = 'Van';
+            el.dispatchEvent(new Event('input'));
+
+            expect(el.value).toBe('Van');
+        })
+    }));
+
+    it('should input value in position', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#position'));
+            let el = input.nativeElement;
+
+            expect(el.value).toBe('');
+
+            el.value = 'Developer';
+            el.dispatchEvent(new Event('input'));
+
+            expect(el.value).toBe('Developer');
+        })
+    }));
+
+    it('should input value in compensation', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#compensation'));
+            let el = input.nativeElement;
+
+            expect(el.value).toBe('');
+
+            el.value = '120000';
+            el.dispatchEvent(new Event('input'));
+
+            expect(el.value).toBe('120000');
+        })
     }));
 });
--- src/app/update-modal/update-modal.component.spec.ts
+++ src/app/update-modal/update-modal.component.spec.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UpdateModalComponent } from './update-modal.component';
+import { By } from '@angular/platform-browser';
 
 @Component({selector: 'app-mat-dialog-ref', template: ''})
 class DialogRef {
@@ -50,4 +51,47 @@ describe('UpdateModalComponent', () => {
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain('Update Compensation');
     }));
+
+    it('should has first name as disabled', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#firstName'));
+            let el = input.nativeElement;
+            el.value = component.employee.firstName;
+
+            expect(el.value).toBe('first');
+            expect(el.disabled).toBeTruthy();
+        })
+    }));
+
+    it('should has last name as disabled', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#lastName'));
+            let el = input.nativeElement;
+            el.value = component.employee.lastName;
+
+            expect(el.value).toBe('last');
+            expect(el.disabled).toBeTruthy();
+        })
+    }));
+
+    it('should input value in compensation', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement;
+        fixture.whenStable().then(() => {
+            let input = compiled.query(By.css('#compensation'));
+            let el = input.nativeElement;
+
+            expect(el.value).toBe('');
+
+            el.value = '120000';
+            el.dispatchEvent(new Event('input'));
+
+            expect(el.value).toBe('120000');
+        })
+    }));
+    
 });
